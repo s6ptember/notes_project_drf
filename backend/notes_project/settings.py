0e6eaac4a3a8ee9370cbbf0e79147b1ef392c52d
september <september@death.com>
2025-08-01 16:48:22 +0300

    add frontend

--- backend/notes_project/settings.py
+++ backend/notes_project/settings.py
@@ -138,8 +138,8 @@ if DEBUG:
     CORS_ALLOW_ALL_ORIGINS = True
 else:
     CORS_ALLOWED_ORIGINS = [ # Разрешенные источники
-        'http://localhost:3000',
-        'http://127.0.0.1:3000',
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
     ]
 
 # Настройки безопасности
